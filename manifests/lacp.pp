7cb469b4ab1cae91612e77d662e0cea160957cd4
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-01-10 15:46:58 +0530

    Add License

--- manifests/lacp.pp
+++ manifests/lacp.pp
@@ -1,3 +1,14 @@
+# Copyright (c) 2017, Lenovo. All rights reserved.
+#
+# This program and the accompanying materials are licensed and made available
+# under the terms and conditions of the 3-clause BSD License that accompanies 
+# this distribution. The full text of the license may be found at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
+# WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
 cnos_lacp {'sys_prio':
 sys_prio => '32769',
 }
