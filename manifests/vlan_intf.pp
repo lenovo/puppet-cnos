7c66644993fefb584720f2e12c77a083c7a2b090
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-27 18:23:18 +0530

    Update vlan_intf.pp

--- manifests/vlan_intf.pp
+++ manifests/vlan_intf.pp
@@ -9,6 +9,22 @@
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
+# This class provides mapping of interface with vlan
+#
+# This is an example of how to make this association
+# Both the interface and the vlans has to be present for this association.
+# If you change ensure to 'absent', it will dissociate from all vlan
+# You can change all the parameters of this association using this class
+#
+# @example Declaring the class
+#   include cnos_vlan_intf
+#
+# @param [String] Ethernet interface name.
+# @property if_name [String] Ethernet interface name.
+# @property bridgeport_mode [String] Bridge Port mode has to be either 'access' or trunk.
+# @property pvid [Integer] The Native VLAN ID for switch ports.
+# @property vlans [Array] It has to be an array of vlan id or the string 'all' or 'none'.
+
 class cnos::vlan_intf {
   cnos_vlan_intf { 'Ethernet1/92':
     ensure          => 'present',
