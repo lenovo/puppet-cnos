a31859bf23ad09e66abb8b68dc219aafb399c252
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-27 16:54:35 +0530

    Review comments incorporated

--- manifests/vlan.pp
+++ manifests/vlan.pp
@@ -1,4 +1,4 @@
-# Copyright (c) 2017, Lenovo. All rights reserved.
+# Copyright (c) 2018, Lenovo. All rights reserved.
 #
 # This program and the accompanying materials are licensed and made available
 # under the terms and conditions of the 3-clause BSD License that accompanies 
@@ -8,10 +8,25 @@
 #
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+#
+# A Vlan creation/update/delete class for CNOS Devices.
+#
+# This is an example of how to create a vlan
+# If you change ensure to absent, it will delete the vlan
+# You can change vlan_name and admin_state of a device using this class
+#
+# @example Declaring the class
+#   include cnos::vlan
+#
+# @param [String] Vlan id of the Vlan.
+# @property [Integer] Vlan id of the Vlan.
+# @property [String] Admin state of this vlan.
+# @property [String] Name of this vlan.
 
 class cnos::vlan {
   cnos_vlan { '11':
     ensure      => 'present',
+    vlan_id     => 11,
     admin_state => 'down',
     vlan_name   => 'test11',
   }
