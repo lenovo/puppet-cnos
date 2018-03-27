ed856f2722ec867858941f1ed1fc54c7ce8a958e
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-27 23:05:31 +0530

    Update vrrp.pp

--- manifests/vrrp.pp
+++ manifests/vrrp.pp
@@ -8,6 +8,31 @@
 #
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+# A Vlan creation/update/delete class for CNOS Devices.
+#
+# This is an example of how to create a VRRP
+# If you change ensure to 'absent', it will delete the VRRP Configuration
+# You can change various attributes associated with VRRP using this class
+#
+# @example Declaring the class
+#   include cnos_vrrp
+#
+# @param [String] name vr_id and if_name as String
+# @property [String] if_name Interface name.
+# @property [Integer] vr_id Virtual Router (VR) identifier; an integer from 1-255.
+# @property [String] ip_addr The IP address of the VR; a valid IPv4 address.
+# @property [Integer] ad_intvl Advertisement interval in centi seconds.
+# @property [String] preempt Enable the preemption of a lower priority master. yes or no
+# @property [Integer] prio Priority of the VR on the switch.
+# @property [String] admin_state Admin state of this vlan.
+# @property [String] oper_state The operation state of the VR.
+# @property [String] track_if The interface to track by this VR.
+# @property [String] accept_mode Enables or disables the accept mode.
+# @property [Integer] switch_back_delay The switch back delay interval.
+# @property [String] v2_compt Enables backward compatibility for VRRPv2.
+
+
+# @property [String] vlan_name Name of this vlan.
 
 class cnos::vrrp {
   cnos_vrrp{ '1' :
