22b80593237b8ec6b0322603b0391100f8719451
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-27 14:47:11 +0530

    Removing commented code

--- lib/puppet/util/network_device/cnos/facts.rb
+++ lib/puppet/util/network_device/cnos/facts.rb
@@ -10,16 +10,13 @@ class Puppet::Util::NetworkDevice::Cnos::Facts
     Puppet.debug(self.class.to_s.split('::').last + ': Retrieving Facts from facts.rb!')
     facts = {}
     facts.merge(parse_device_facts)
-    # facts = parse_device_facts()
   end
 
   def parse_device_facts
     facts = {
       operatingsystem: :cnos
     }
-    # if response = @transport.call('/nos/api/system/')
     if response = @transport.call('/nos/api/sysinfo/inventory')
-      # result = items.first
       Puppet.debug("response  = #{response}")
       result = response
     else
@@ -27,9 +24,6 @@ class Puppet::Util::NetworkDevice::Cnos::Facts
       return facts
     end
 
-    # [ :switch_type,
-    #  :fw_version
-    # ].each do |fact|
     [:Name,
      :Description,
      :Model,
@@ -41,13 +35,10 @@ class Puppet::Util::NetworkDevice::Cnos::Facts
      :'Mgmt mac',
      :'Machine Type Model',
      :UpTime].each do |fact|
-      # Puppet.debug("response  = #{result[fact.to_s]}")
       facts[fact] = result[fact.to_s]
     end
 
     # Map CNOS names to expected standard names.
-    # facts[:switch_type]            = facts[:switch_type]
-    # facts[:fw_version]             = facts[:fw_version]
     facts[:Name]                       = facts[:Name]
     facts[:Description]                = facts[:Description]
     facts[:Model]                      = facts[:Model]
