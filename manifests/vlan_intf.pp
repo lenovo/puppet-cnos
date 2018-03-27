3357cd86ece308f2dfa3e3929a10e740a8675529
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-27 17:48:00 +0530

    Update vlan_intf.pp

--- manifests/vlan_intf.pp
+++ manifests/vlan_intf.pp
@@ -12,6 +12,7 @@
 class cnos::vlan_intf {
   cnos_vlan_intf { 'Ethernet1/92':
     ensure          => 'present',
+    if_name         => 'Ethernet1/92',
     bridgeport_mode => 'trunk',
     pvid            => 1,
     vlans           => [10],
