f8b8d32b8a9fa0ce33c3b29fe39af82c085d060f
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-28 18:28:16 +0530

    Updating documentation

--- manifests/vlag_isl.pp
+++ manifests/vlag_isl.pp
@@ -9,6 +9,20 @@
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
+# This class provides setting of VLAG Instance configurations
+#
+# This is an example to show how to set the port aggregator 
+# This class allows you to set port-aggregator value only.
+# Rest of the attributes including delete options will 
+# be implemented in later releases to this class.
+#
+# @example Declaring the class
+#   include cnos_vlag_isl
+#
+# @param [String] name Its defined as vlag_isl.
+# @property [Integer] msg_interval The interval with which heartbeat messages sent.
+# @property [Integer] port_aggregator LAG identifier; an integer from 1-4096.
+
 class cnos::vlag_isl {
   cnos_vlag_isl {'vlag_isl':
     port_aggregator => 10
