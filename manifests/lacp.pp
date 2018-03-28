5833ee28fb12979f4e6b4cec2f5dd6caa510ffc7
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-28 15:50:53 +0530

    Adding documentation

--- manifests/lacp.pp
+++ manifests/lacp.pp
@@ -9,8 +9,20 @@
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
+# This class provides setting of LACP system priority for all interfaces
+#
+# This is an example of how to make this sys_prio value to set
+# If you change ensure to 'absent', it will reset this value to factory default
+# The default value is 32768
+#
+# @example Declaring the class
+#   include cnos_lacp
+#
+# @property [Integer] sys_prio LACP system priority, a positive integer from 1-65535.
+
 class cnos::lacp {
   cnos_lacp {'sys_prio':
+    ensure   => 'present',
     sys_prio => '32769',
   }
 }
