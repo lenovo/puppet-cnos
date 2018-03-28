014d6a7a0e00a724c1ab7c793105b2a765a8dbbd
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-28 18:49:49 +0530

    Update lag.pp

--- manifests/lag.pp
+++ manifests/lag.pp
@@ -9,6 +9,24 @@
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
+# A LAG creation/update/delete class for CNOS Devices.
+#
+# This is an example of how to create a LAG
+# If you change ensure to 'absent', it will delete the LAG Configuration
+# You can change various attributes associated with LAG using this class
+#
+# @example Declaring the class
+#   include cnos_lag
+#
+# @param [String] name lag_id as String
+# @property [String] lag_id LAG identifier; an integer from 1-65535
+# @property [Integer] min_links LACP minimum links number; an integer from 1-32
+# @property [Integer] lacp_prio LACP priority for the physical port
+# @property [String] lacp_timeout LACP timeout for the physical port.
+# @property [String] lag_mode LAG mode; one of lacp_active, lacp_passive, no_lacp.
+# @property [String] if_name Ethernet interface name.
+
+
 class cnos::lag {
   cnos_lag { '11':
     ensure     => 'present',
@@ -18,6 +36,7 @@ class cnos::lag {
       'lag_mode'     => 'lacp_active',
       'if_name'      => 'Ethernet1/23',
     }],
+    lag_id     => 11,
     min_links  => 2,
   }
 }
