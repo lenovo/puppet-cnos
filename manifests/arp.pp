e536633de53fb1b2272f0176fc5b1f679ae90585
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-28 15:21:26 +0530

    Updating class documentation

--- manifests/arp.pp
+++ manifests/arp.pp
@@ -9,6 +9,19 @@
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
+# This class provides setting of ageout time for arp for an interface
+#
+# This is an example of how to make this timeout value to set
+# The interface has to be present for this setting.
+# If you change ensure to 'absent', it will reset this value to factory default
+# The default value is 1500
+#
+# @example Declaring the class
+#   include cnos_arp
+#
+# @param [String] name Ethernet interface name.
+#@property [Integer] ageout_time The global ARP entry age-out time, in seconds.
+
 class cnos::arp {
   cnos_arp{'Ethernet1/1':
     ageout_time => 80,
