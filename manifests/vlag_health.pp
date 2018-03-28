47b0f01139320ef8de72d65e00f14d622896d89c
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-28 18:00:50 +0530

    Update vlag_health.pp

--- manifests/vlag_health.pp
+++ manifests/vlag_health.pp
@@ -9,9 +9,25 @@
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
+# This class is used in configurations of VLAG Health Check parameters
+#
+# This is an example of how to make this configirations
+# All attributes are not mandatory for this configurations
+# If you change ensure to 'absent', everything will revert to default values
+# You can change all the parameters of VLAG configurations using this class
+#
+# @example Declaring the class
+#   include cnos_vlag_hc
+#
+# @param [String] name The string vlag_health.
+# @property [String] status Whether the vLAG is enabled or disabled.
+# @property [Integer] keepalive_interval Time interval, in seconds.
+# @property [Integer] retry_interval Time interval, in seconds.
+# @property [Integer] keepalive_attempts Number of keepalive attempts before declaring peer down.
+
 class cnos::vlag_health {
-  cnos_vlag_hc {'vlag':
-    ensure             => absent,
+  cnos_vlag_hc {'vlag_health':
+    ensure             => 'absent',
     keepalive_interval => 5,
     retry_interval     => 10,
     keepalive_attempts => 15,
