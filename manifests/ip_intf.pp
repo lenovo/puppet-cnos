cd4494684b43a0fd982a02b709d6090c52c1656c
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-29 10:18:00 +0530

    Update documentation

--- manifests/ip_intf.pp
+++ manifests/ip_intf.pp
@@ -9,6 +9,25 @@
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
+# This class provides ip interface association
+#
+# This is an example of how to make this association
+# The interface must exist for this association.
+# If you change ensure to 'absent', it will just change mtu and bridge_port 
+# to default values. There is no delete method for this association
+# You can change all the parameters of this association using this class
+#
+# @example Declaring the class
+#   include cnos_ip_intf
+#
+# @param [String] name Ethernet interface name.
+# @property [String] if_name Ethernet interface name. The interface must exist.
+# @property [String] ip_addr IP address for the interface.
+# @property [String] bridge_port Whether or not the port is a bridge port
+# @property [Integer] mtu The maximum transmission unit, in bytes
+# @property [String] vrf_name The name of the VRF to which the interface belongs.The named VRF must exist.
+# @property [Integer] ip_prefix_len IP address mask, a positive integer from 1-32.
+# @property [String] admin_state The admin status; one of up, down.
 class cnos::ip_intf {
   cnos_ip_intf{'Ethernet1/11':
     ensure        => present,
