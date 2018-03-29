e45e8d923fd1993e5d5fb36904df1c1ab54b99f5
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-29 10:50:08 +0530

    Adding documentation

--- manifests/bst_feature.pp
+++ manifests/bst_feature.pp
@@ -9,6 +9,25 @@
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
+# This class is used to get and set BST Feature 
+#
+# This is an example of how to make this telemtry configuration.
+# If you change ensure to 'absent', it will set all the values to device defaults.
+# You can change all the parameters of this association using this class
+#
+# @example Declaring the class
+#   include cnos_telemetry
+#
+# @param [String] name The value is bst_feature.
+# @property [Integer] collection_interval The collection interval, in seconds
+# @property [Integer] send_async_reports To enable the transmission of periodic asynchronous reports
+# @property [Integer] send_snapshot_on_trigger To enable sending a complete snapshot
+# @property [Integer] trigger_rate_limit The trigger rate limit,
+# @property [Integer] async_full_report Set the async full report feature
+# @property [Integer] trigger_rate_limit_interval The trigger rate limit interval
+# @property [Integer] bst_enable Set to 1 to enable BST, 0 to disable it.
+
+
 class cnos::bst_feature {
   cnos_telemetry{ 'bst_feature':
     ensure                      => 'present',
