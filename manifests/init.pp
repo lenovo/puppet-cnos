6c9fe8baff950ed34c80a6b8714fd372d601f621
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-06 09:28:43 +0530

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,12 +10,5 @@
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 class cnos {
-  cnos_vlan { '10':
-    ensure      => 'present',
-    admin_state => 'down',
-    vlan_name   => 'test10',}
-  cnos_vlan { '11':
-    ensure      => 'present',
-    admin_state => 'up',
-    vlan_name   => 'test11',}
+  class { 'cnos::install': }
 }
