ebee9e79c18e541bf143c5239ce93bc0448a7b2e
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-06 09:31:34 +0530

    Create install.pp

---
+++ manifests/install.pp
@@ -0,0 +1,14 @@
+# Private class
+class cnos::install {
+  if $::puppetversion and $::puppetversion =~ /Puppet Enterprise/ {
+    $provider = 'pe_gem'
+  } elsif $::puppetversion and versioncmp($::puppetversion, '4.0.0') >= 0 {
+    $provider = 'puppet_gem'
+  } else {
+    $provider = 'gem'
+  }
+  package { 'lenovo-rbapi':
+    ensure   => '0.0.5',
+    provider => $provider,
+  }
+}
