4ac4b9bc09e1b8d1d61e7595e5a550468c7d5cdd
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-22 15:54:07 +0530

    Update cnos_vrrp_spec.rb

--- spec/defines/cnos_vrrp_spec.rb
+++ spec/defines/cnos_vrrp_spec.rb
@@ -31,11 +31,11 @@ describe 'cnos_vrrp', type: :type do
     end
   }
 
-#  it 'should have expected properties' do
-#    properties.each do |property|
-#      expect(type_class.properties.map(&:name)).to be_include(property)
-#    end
-#  end
+  #  it 'should have expected properties' do
+  #    properties.each do |property|
+  #      expect(type_class.properties.map(&:name)).to be_include(property)
+  #    end
+  #  end
 
   it 'should have expected parameters' do
     params.each do |param|
