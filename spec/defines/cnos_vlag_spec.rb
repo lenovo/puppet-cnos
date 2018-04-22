9844f2b38e273efcb9d1c508322a3e4b7bb4c5ea
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-22 07:40:06 +0530

    Update cnos_vlag_spec.rb

--- spec/defines/cnos_vlag_spec.rb
+++ spec/defines/cnos_vlag_spec.rb
@@ -23,6 +23,12 @@ describe 'cnos_vlag', type: :type do
     end
   }
 
+#  it 'should have expected properties' do
+#    properties.each do |property|
+#      expect(type_class.properties.map(&:name)).to be_include(property)
+#    end
+#  end
+
   it 'should have expected parameters' do
     params.each do |param|
       expect(type_class.parameters).to be_include(param)
