3a618a7613283c95627c441d904d35405defb8f7
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-22 07:39:09 +0530

    Update cnos_vlag_hc_spec.rb

--- spec/defines/cnos_vlag_hc_spec.rb
+++ spec/defines/cnos_vlag_hc_spec.rb
@@ -25,6 +25,11 @@ describe 'cnos_vlag_hc', type: :type do
     end
   }
 
+#  it 'should have expected properties' do
+#    properties.each do |property|
+#      expect(type_class.properties.map(&:name)).to be_include(property)
+#    end
+#  end
 
   it 'should have expected parameters' do
     params.each do |param|
