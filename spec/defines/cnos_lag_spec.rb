8479a442b84838f13e7d797665182441ec280122
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-22 15:46:52 +0530

    Update cnos_lag_spec.rb

--- spec/defines/cnos_lag_spec.rb
+++ spec/defines/cnos_lag_spec.rb
@@ -23,11 +23,11 @@ describe 'cnos_lag', type: :type do
     end
   }
 
-#  it 'should have expected properties' do
-#    properties.each do |property|
-#      expect(type_class.properties.map(&:name)).to be_include(property)
-#    end
-#  end
+  #  it 'should have expected properties' do
+  #    properties.each do |property|
+  #      expect(type_class.properties.map(&:name)).to be_include(property)
+  #    end
+  #  end
 
   it 'should have expected parameters' do
     params.each do |param|
