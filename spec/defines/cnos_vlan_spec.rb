b8c52b247a91c80ffc78115f0265e097001c318a
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-23 15:13:36 +0530

    Replacing should with does

--- spec/defines/cnos_vlan_spec.rb
+++ spec/defines/cnos_vlan_spec.rb
@@ -17,31 +17,31 @@ describe 'cnos_vlan', type: :type do
     ]
   end
 
-  it {
-    is_expected.to be_valid_type.with_properties do
+  # it {
+  #   is_expected.to be_valid_type.with_properties do
+  #     expect(type_class.properties.map(&:name)).to be_include(property)
+  #   end
+  # }
+
+  it 'does have expected properties' do
+    properties.each do |property|
       expect(type_class.properties.map(&:name)).to be_include(property)
     end
-  }
+  end
 
-  #  it 'should have expected properties' do
-  #    properties.each do |property|
-  #      expect(type_class.properties.map(&:name)).to be_include(property)
-  #    end
-  #  end
-
-  it 'should have expected parameters' do
+  it 'does have expected parameters' do
     params.each do |param|
       expect(type_class.parameters).to be_include(param)
     end
   end
 
-  it 'should require a name' do
+  it 'does require a name' do
     expect do
       type_class.new({})
     end.to raise_error(Puppet::Error, 'Title or name must be provided')
   end
 
-  it 'should support :present as a value to :ensure' do
+  it 'does support :present as a value to :ensure' do
     type_class.new(name: 'A great VLAN', vlan_id: '20', ensure: :present)
   end
 end
