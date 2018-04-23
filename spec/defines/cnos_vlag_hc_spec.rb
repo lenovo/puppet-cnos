1e75e0f2d4cdde24d573ebddae5e9e76b5638024
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-23 15:07:39 +0530

    Replacing should with does

--- spec/defines/cnos_vlag_hc_spec.rb
+++ spec/defines/cnos_vlag_hc_spec.rb
@@ -31,19 +31,19 @@ describe 'cnos_vlag_hc', type: :type do
     end
   end
 
-  it 'should have expected parameters' do
+  it 'does have expected parameters' do
     params.each do |param|
       expect(type_class.parameters).to be_include(param)
     end
   end
 
-  it 'should require a name' do
+  it 'does require a name' do
     expect do
       type_class.new({})
     end.to raise_error(Puppet::Error, 'Title or name must be provided')
   end
 
-  it 'should support :present as a value to :ensure' do
+  it 'does support :present as a value to :ensure' do
     type_class.new(name: 'VLAG Health Check', title: 'vlag', ensure: :present)
   end
 end
