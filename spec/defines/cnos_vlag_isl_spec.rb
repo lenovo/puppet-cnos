0557a8da974fc8ecd6e09a9cb6a720fd6230de1a
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-23 15:09:54 +0530

    Replacing should with does

--- spec/defines/cnos_vlag_isl_spec.rb
+++ spec/defines/cnos_vlag_isl_spec.rb
@@ -15,31 +15,31 @@ describe 'cnos_vlag_isl', type: :type do
     ]
   end
 
-  it {
-    is_expected.to be_valid_type.with_properties do
+  # it {
+  #   is_expected.to be_valid_type.with_properties do
+  #     expect(type_class.properties.map(&:name)).to be_include(property)
+  #   end
+  # }
+
+  it 'does have expected properties' do
+    properties.each do |property|
       expect(type_class.properties.map(&:name)).to be_include(property)
     end
-  }
+  end
 
-  #  it 'should have expected properties' do
-  #    properties.each do |property|
-  #      expect(type_class.properties.map(&:name)).to be_include(property)
-  #    end
-  #  end
-
-  it 'should have expected parameters' do
+  it 'does have expected parameters' do
     params.each do |param|
       expect(type_class.parameters).to be_include(param)
     end
   end
 
-  it 'should require a name' do
+  it 'does require a name' do
     expect do
       type_class.new({})
     end.to raise_error(Puppet::Error, 'Title or name must be provided')
   end
 
-  it 'should support :present as a value to :ensure' do
+  it 'does support :present as a value to :ensure' do
     type_class.new(name: 'VLAG ISL Configuration',
                    title: 'vlag_isl', ensure: :present)
   end
