343f1edbb4a22a83d144d18bca64bfc31135de50
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-24 22:31:02 +0530

    Update vlan.pp

--- examples/vlan.pp
+++ examples/vlan.pp
@@ -1,8 +1,10 @@
 # CNOS Vlan example
 
 class cnos::vlan {
-  ensure      => 'present',
-  vlan_id     => 11,
-  admin_state => 'up',
-  vlan_name   => 'Vlan 11',
+  cnos_vlan { 'Vlan 11':
+    ensure      => 'present',
+    vlan_id     => 11,
+    admin_state => 'up',
+    vlan_name   => 'Vlan 11',
+  }
 }
