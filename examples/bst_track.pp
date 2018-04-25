a8f9319392733b9ba26b3ba75c63f6af94504bd6
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-25 07:52:38 +0530

    Create bst_track.pp

---
+++ examples/bst_track.pp
@@ -0,0 +1,17 @@
+# Docu
+
+class cnos::bst_track {
+  cnos_telemetry_track{'bst_track':
+    track_egress_port_service_pool    =>  0,
+    track_egress_uc_queue             =>  0,
+    track_egress_rqe_queue            =>  0,
+    track_egress_cpu_queue            =>  0,
+    track_ingress_port_service_pool   =>  0,
+    track_ingress_service_pool        =>  0,
+    track_egress_mc_queue             =>  0,
+    track_peak_stats                  =>  1,
+    track_ingress_port_priority_group =>  0,
+    track_egress_service_pool         =>  0,
+    track_device                      =>  0,
+  }
+}
