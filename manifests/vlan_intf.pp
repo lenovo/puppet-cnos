2f393cc9eb36dba0012a9e638c85e97c79b029c8
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-25 10:00:22 +0530

    Update vlan_intf.pp

--- manifests/vlan_intf.pp
+++ manifests/vlan_intf.pp
@@ -19,18 +19,24 @@
 # @example Declaring the class
 #   include cnos_vlan_intf
 #
-# @param [String] name Ethernet interface name.
+# @param [String] if_name Ethernet interface name.
 # @property [String] if_name Ethernet interface name.
 # @property [String] bridgeport_mode Bridge Port mode has to be either 'access' or trunk.
 # @property [Integer] pvid The Native VLAN ID for switch ports.
 # @property [Array] vlans It has to be an array of vlan id or the string 'all' or 'none'.
 
-class cnos::vlan_intf {
-  cnos_vlan_intf { 'Ethernet1/92':
-    ensure          => 'present',
-    if_name         => 'Ethernet1/92',
-    bridgeport_mode => 'trunk',
-    pvid            => 1,
-    vlans           => [10],
+class cnos::vlan_intf (
+  String $bridgeport_mode,
+  Integer $pvid,
+  Array $vlans,
+  String $if_name = $title,
+  Enum['present', 'absent'] $ensure  = 'present',
+){
+  cnos_vlan_intf { $if_name :
+    ensure          => $ensure,
+    if_name         => $if_name,
+    bridgeport_mode => $bridgeport_mode,
+    pvid            => $pvid,
+    vlans           => $vlans,
   }
 }
