b606d4309ad646e1472ec32972c4ff7dc7fbb397
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-25 08:00:44 +0530

    Create lag.pp

---
+++ examples/lag.pp
@@ -0,0 +1,15 @@
+# CNOS LAG example
+
+class cnos::lag {
+  cnos_lag { '11':
+    ensure     => 'present',
+    interfaces =>  [ {
+      'lacp_prio'    => 32768,
+      'lacp_timeout' => 'long',
+      'lag_mode'     => 'lacp_active',
+      'if_name'      => 'Ethernet1/23',
+    }],
+    lag_id     => 11,
+    min_links  => 2,
+  }
+}
