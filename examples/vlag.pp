4546adac9fad98c926874e41c59bcbc66fa79fb1
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-04-25 10:16:10 +0530

    Update vlag.pp

--- examples/vlag.pp
+++ examples/vlag.pp
@@ -2,8 +2,8 @@
 
 class cnos::vlag {
   cnos_vlag { '2':
-    inst_id         => 2,
     ensure          => 'present',
+    inst_id         => 2,
     status          => 'disable',
     port_aggregator => 20,
   }
