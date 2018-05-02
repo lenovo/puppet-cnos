4fdc3b5656720fd00f28305730658e05eaf2e8ef
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-05-02 20:36:43 +0530

    Removing the example section from documentation

--- manifests/bst_track.pp
+++ manifests/bst_track.pp
@@ -3,17 +3,13 @@
 # This program and the accompanying materials are licensed and made available
 # under the terms and conditions of the 3-clause BSD License that accompanies 
 # this distribution. The full text of the license may be found at
-
+#
 # https://opensource.org/licenses/BSD-3-Clause
-
+#
 # THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
 # WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
-# This class is used to get and set BST Tracking Parameters 
 #
-# This is an example of how to make this telemtry track configuration.
-# If you change ensure to 'absent', it will set all the values to device defaults.
-# You can change all the parameters of this association using this class
+# This class is used to get and set BST Tracking Parameters 
 #
 # @example Declaring the class
 #   include cnos_telemetry_track
