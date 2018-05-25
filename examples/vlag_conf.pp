f1f98b5116521b45a5f00a5f5e362a10bed1a08b
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-05-25 19:56:20 +0530

    Final Change

--- examples/vlag_conf.pp
+++ examples/vlag_conf.pp
@@ -1,12 +1,10 @@
 # CNOS Vlag Configuration example
 
-class cnos::vlag_conf {
-  cnos_vlag_conf{'vlag_conf':
-    ensure        => 'present',
-    status        => 'enable',
-    tier_id       => 10,
-    priority      => 33,
-    auto_recover  => 313,
-    startup_delay => 133,
-  }
+cnos_vlag_conf{' vlag_conf' :
+  ensure        => 'present',
+  status        => 'enable',
+  tier_id       => 10,
+  priority      => 33,
+  auto_recover  => 313,
+  startup_delay => 133,
 }
