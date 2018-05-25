cdd192df17d66274f259dab121ef98b52be3afde
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-05-25 19:52:23 +0530

    Final Change

--- examples/lacp.pp
+++ examples/lacp.pp
@@ -1,8 +1,6 @@
 # CNOS LACP example
 
-class cnos::lacp {
-  cnos_lacp {'sys_prio':
-    ensure   => 'present',
-    sys_prio => '32769',
-  }
+cnos_lacp { 'sys_prio' :
+  ensure   => 'present',
+  sys_prio => '32769',
 }
