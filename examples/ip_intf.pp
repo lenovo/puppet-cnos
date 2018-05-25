313bbae9d1ceea55e2e80c91be7f83293b268580
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-05-25 19:50:49 +0530

    Final Change

--- examples/ip_intf.pp
+++ examples/ip_intf.pp
@@ -1,14 +1,12 @@
 # CNOS IP Interface example
 
-class cnos::ip_intf {
-  cnos_ip_intf{'Ethernet1/11':
-    ensure        => present,
-    ip_addr       => '1.1.1.1',
-    bridge_port   => 'no',
-    if_name       => 'Ethernet1/11',
-    mtu           => 1502,
-    vrf_name      => 'default',
-    admin_state   => 'up',
-    ip_prefix_len => 24
-  }
+cnos_ip_intf{ 'Ethernet1/11' :
+  ensure        => present,
+  ip_addr       => '1.1.1.1',
+  bridge_port   => 'no',
+  if_name       => 'Ethernet1/11',
+  mtu           => 1502,
+  vrf_name      => 'default',
+  admin_state   => 'up',
+  ip_prefix_len => 24,
 }
