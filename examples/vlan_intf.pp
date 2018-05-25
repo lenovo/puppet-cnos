d03005b2ca3b7a2b91f1ea9eebb23902ef02dc97
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-05-25 19:59:23 +0530

    Final Change

--- examples/vlan_intf.pp
+++ examples/vlan_intf.pp
@@ -1,11 +1,9 @@
 # CNOS Vlan Interface example
 
-class cnos::vlan_intf {
-  cnos_vlan_intf { 'Ethernet1/92':
-    ensure          => 'present',
-    if_name         => 'Ethernet1/92',
-    bridgeport_mode => 'trunk',
-    pvid            => 1,
-    vlans           => [10],
-  }
+cnos_vlan_intf { 'Ethernet1/92' :
+  ensure          => 'present',
+  if_name         => 'Ethernet1/92',
+  bridgeport_mode => 'trunk',
+  pvid            => 1,
+  vlans           => [10],
 }
