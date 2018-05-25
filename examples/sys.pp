a9c2a4eb0e97817adb7c52156e6b824bd121825e
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-05-25 19:54:15 +0530

    Final Change

--- examples/sys.pp
+++ examples/sys.pp
@@ -1,9 +1,7 @@
 # CNOS BST System example
 
-class cnos::sys {
-  cnos_sys{'sys':
-    ensure           => present,
-    msg_interval     => 15,
-    heartbeat_enable => 1
-  }
+cnos_sys{ 'sys' :
+  ensure           => present,
+  msg_interval     => 15,
+  heartbeat_enable => 1,
 }
