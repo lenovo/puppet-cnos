7e7543e482b51d6a23de493f9d34cf40ca300b53
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-09-28 16:20:02 +0530

    Removing should be

--- lib/puppet/provider/cnos_vlag_isl/gem.rb
+++ lib/puppet/provider/cnos_vlag_isl/gem.rb
@@ -26,8 +26,7 @@ Puppet::Type.type(:cnos_vlag_isl).provide(:gem, parent: Puppet::Provider::Cnos) 
 
   def exists?
     Puppet.debug('I am inside exists')
-    @property_hash[:ensure].should be == :present
-    # @property_hash[:ensure] == :present
+    @property_hash[:ensure] == :present
     # return true since resource is always present
     true
   end
