934ba05c7762a8ea2b5be0134fc93c658f79c5bb
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-11-16 12:21:44 +0530

    Update gem.rb

--- lib/puppet/provider/cnos_telemetry/gem.rb
+++ lib/puppet/provider/cnos_telemetry/gem.rb
@@ -67,7 +67,8 @@ Puppet::Type.type(:cnos_telemetry).provide(:gem, parent: Puppet::Provider::Cnos)
 
   def exists?
     Puppet.debug('I am inside exists')
-    @property_hash[:ensure].should eq(:present)
+    @property_hash[:ensure] == :present
+    # @property_hash[:ensure].should eq(:present)
     true
   end
 
